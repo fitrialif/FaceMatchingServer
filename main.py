bfa450c5f447e39404632930d69bc63aaaea789c
Nicolas Debernardi <nicolas.debernardi@gmail.com>
2018-03-20 00:21:15 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ K.set_image_data_format('channels_first')
 import tensorflow as tf
 from fr_utils import *
 from inception_blocks import *
-from LoadModel import *
+#from LoadModel import *
+FRmodel = True
 
 def verify(image_path_1, image_path_2, model):
     """
@@ -25,22 +26,23 @@ def verify(image_path_1, image_path_2, model):
 
     ### START CODE HERE ###
 
-    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
-    encoding_1 = img_to_encoding(image_path_1, model)
-    encoding_2 = img_to_encoding(image_path_2, model)
-
-    # Step 2: Compute distance with identity's image (≈ 1 line)
-    dist = np.linalg.norm(encoding_1 - encoding_2)
-
-    # Step 3: Match faces if dist < 0.7, else don't match
-    if dist < 0.7:
-        same_face = True
-    else:
-        same_face = False
+    # # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
+    # encoding_1 = img_to_encoding(image_path_1, model)
+    # encoding_2 = img_to_encoding(image_path_2, model)
+    #
+    # # Step 2: Compute distance with identity's image (≈ 1 line)
+    # dist = np.linalg.norm(encoding_1 - encoding_2)
+    #
+    # # Step 3: Match faces if dist < 0.7, else don't match
+    # if dist < 0.7:
+    #     same_face = True
+    # else:
+    #     same_face = False
 
     ### END CODE HERE ###
 
-    return dist, same_face
+    #return dist, same_face
+    return True
 
 PWD = '/var/www/html/flaskapp'
 MODEL_PATH = PWD + '/data/output_graph.pb'
@@ -52,11 +54,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-cors = CORS(app, resources={r"/api/photo-recognize": {"origins": "http://localhost:port"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/photo-recognize', methods=['POST'])
 def photoRecognize():
-
+    print("coolos")
+    #print(request)
+    for key in request.form.keys():
+        print(key)
     jsdata_1 = request.form['canvas_data_1']
     jsdata_2 = request.form['canvas_data_2']
 
