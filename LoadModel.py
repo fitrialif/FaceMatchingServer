82bd45ce12c597018dd96cb35301fa7b3711c874
Nicolas Debernardi <nicolas.debernardi@gmail.com>
2018-03-19 23:55:07 +0100

    Initial commit

---
+++ LoadModel.py
@@ -0,0 +1,43 @@
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+import tensorflow as tf
+from fr_utils import *
+from inception_blocks import *
+
+FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+
+    Returns:
+    loss -- real number, value of the loss
+    """
+
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    ### START CODE HERE ### (≈ 4 lines)
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), keepdims=True)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), keepdims=True)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+    ### END CODE HERE ###
+
+    return loss
+
+## 2 - Loading the trained model
+#FaceNet is trained by minimizing the triplet loss. But since training requires a lot of data and a lot of computation, we won't train it from scratch here. Instead, we load a previously trained model. Load a model using the following cell; this might take a couple of minutes to run.
+FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
+load_weights_from_FaceNet(FRmodel)
---
+++ main.py
@@ -0,0 +1,81 @@
+from sys import platform as _platform
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
+
+from keras import backend as K
+K.set_image_data_format('channels_first')
+import tensorflow as tf
+from fr_utils import *
+from inception_blocks import *
+from LoadModel import *
+
+def verify(image_path_1, image_path_2, model):
+    """
+    Function that verifies if the person on the "image_path" image is "identity".
+
+    Arguments:
+    image_path_1 -- path to an image
+    image_path_2 -- path to an image
+    model -- your Inception model instance in Keras
+
+    Returns:
+    dist -- distance between the 2 image_path_ and _2
+    same_face -- True, if the faces match. False otherwise.
+    """
+
+    ### START CODE HERE ###
+
+    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
+    encoding_1 = img_to_encoding(image_path_1, model)
+    encoding_2 = img_to_encoding(image_path_2, model)
+
+    # Step 2: Compute distance with identity's image (≈ 1 line)
+    dist = np.linalg.norm(encoding_1 - encoding_2)
+
+    # Step 3: Match faces if dist < 0.7, else don't match
+    if dist < 0.7:
+        same_face = True
+    else:
+        same_face = False
+
+    ### END CODE HERE ###
+
+    return dist, same_face
+
+PWD = '/var/www/html/flaskapp'
+MODEL_PATH = PWD + '/data/output_graph.pb'
+LABELS_PATH = PWD + '/data/output_labels.txt'
+
+# HTTP API
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/api/photo-recognize": {"origins": "http://localhost:port"}})
+
+@app.route('/api/photo-recognize', methods=['POST'])
+def photoRecognize():
+
+    jsdata_1 = request.form['canvas_data_1']
+    jsdata_2 = request.form['canvas_data_2']
+
+    # convert string of image data to uint8
+    nparr_1 = np.fromstring(jsdata_1, np.uint8)
+    nparr_2 = np.fromstring(jsdata_2, np.uint8)
+
+    # decode image
+    img_1 = cv2.imdecode(nparr_1, cv2.IMREAD_COLOR)
+    img_2 = cv2.imdecode(nparr_2, cv2.IMREAD_COLOR)
+
+    answer = verify(img_1,img_2, FRmodel)
+
+    ## TODO: do some logic with answer here
+    return jsonify(status='OK', results=answer)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
